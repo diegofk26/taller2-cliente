d4abd5a9f00619764ab019c4c90f19add1c56fa7
seba0703 <sebastian.a.rocha@hotmail.com>
2016-04-29 08:52:51 -0300

    icon + nameApp

--- app/src/main/java/com/example/sebastian/tindertp/LoginActivity.java
+++ app/src/main/java/com/example/sebastian/tindertp/LoginActivity.java
@@ -1,6 +1,5 @@
 package com.example.sebastian.tindertp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
